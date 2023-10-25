964daece5cb600f81af14b78d31716ed21d79aef
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2023-10-25 16:50:36 +0700

    first commit

---
+++ app/src/main/java/com/example/kotlin_io/Data/DataForm.kt
@@ -0,0 +1,5 @@
+package com.example.kotlin_io.Data
+
+data class DataForm(
+    val sex : String = ""
+)
---
+++ app/src/main/java/com/example/kotlin_io/Data/DataSource.kt
@@ -0,0 +1,10 @@
+package com.example.kotlin_io.Data
+
+import com.example.kotlin_io.R
+
+object DataSource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
