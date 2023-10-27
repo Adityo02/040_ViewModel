6b63fb8948c1a5ac74098d9ff9a8799c5e8608dd
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2023-10-27 09:13:56 +0700

    Menambahkan Text Register

--- app/src/main/java/com/example/kotlin_io/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_io/MainActivity.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -74,6 +75,13 @@ fun TampilanLayout(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(20.dp)
         ){
+            Text(
+                text = "Register",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+
             TampilanForm()
         }
     }
